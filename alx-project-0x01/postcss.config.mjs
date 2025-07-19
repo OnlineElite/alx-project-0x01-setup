202b5005d010e2569d4dbb43ebc6e9895f7246c2
OnlineElite <online.0.elite@gmail.com>
2025-07-19 21:52:53 +0100

    fix tailwindcss configuration

--- alx-project-0x01/postcss.config.mjs
+++ alx-project-0x01/postcss.config.mjs
@@ -1,11 +1,17 @@
-module.exports = {
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;
+
+// postcss.config.mjs - Fixed ES module syntax
+/* const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-/* const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
 
 export default config; */
+
+
